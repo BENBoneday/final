d66b138a2ba261653178ab8c337dfccd694fd6d7
BENBoneday <85631569+BENBoneday@users.noreply.github.com>
2021-06-14 18:55:41 +0800

    added a file

---
+++ game.h
@@ -0,0 +1,15 @@
+#pragma once
+#define ROW 3
+#define COL 3
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void InitBoard(char board[ROW][COL],int row,int col);
+void DisplayBoard(char Board[ROW][COL], int row, int col);
+void PlayerMove(char board[ROW][COL], int row, int col);
+void ComputerMove(char board[ROW][COL], int row, int col);
+
+char IsWin(char board[ROW][COL], int row, int col);
+int IsFull(char board[ROW][COL], int row, int col);
---
+++ test_12.c
@@ -0,0 +1,46 @@
+//#define  _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//void bubble_sort(int arr[],int sz)
+//{
+//	int temp;
+//	int flag=1;
+//	for (int i = 1; i < sz; i++)
+//	{
+//		for (int j = 0; j < i; j++)
+//		{
+//			if (arr[i] < arr[j])
+//			{
+//				temp = arr[i];
+//				arr[i] = arr[j];
+//				arr[j] = temp;
+//				flag = 0;
+//			}
+//		}
+//		if (flag == 1)
+//		{
+//			return 0;
+//		}
+//
+//	}
+//}
+//int main()
+//{
+//	int arr[] = { 9,8,7,6,5,4,3,2,1 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bubble_sort(arr,sz);
+//	for (int i = 0; i < 9; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//
+//	///*int arr[3][3] = { {0,1,2},{3,4,5},{6,7} };
+//	//for (int i = 0; i < 3; i++)
+//	//{
+//	//	for (int j = 0; j < 3; j++)
+//	//	{
+//	//		printf("%d ", arr[i][j]);
+//	//	}
+//	//	printf("\n");*/
+//	//}
+//	return 0;
+//}
