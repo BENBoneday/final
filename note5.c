d95abb0f68cbc32a52e683597440fcd659d040ae
BENBoneday <85631569+BENBoneday@users.noreply.github.com>
2021-08-10 22:20:58 +0800

    结构体和大小端

---
+++ note5.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int check_sys()
+{
+	int a = 1;
+	return *(char*)&a;
+}
+int main()
+{
+	int ret = check_sys();
+	if (ret == 1)
+	{
+		printf("Ð¡¶Ë\n ");
+	}
+	else
+	{
+		printf("´ó¶Ë\n ");
+	}
+	return 0;
+}
