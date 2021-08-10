e01bd16f27e27a27128a279ffceb61bb2b9d5d4d
BENBoneday <85631569+BENBoneday@users.noreply.github.com>
2021-08-10 22:24:27 +0800

    结构体

---
+++ contuctor.c
@@ -0,0 +1,29 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<math.h>
+//int sqrtjudge(int num)
+//{
+//	int temp = 0;
+//	int temp1 = 0;
+//	temp = (int)sqrt(num+100.00);
+//	temp1 = (int)sqrt(num + 168.00);
+//	if (((temp * temp) == (num + 100)) && ((temp1 * temp1) == (num + 168)))
+//	{
+//		return 1;
+//	}
+//	else return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i <= 100000; i++)
+//	{
+//		int ret = sqrtjudge(i);
+//		if (ret == 1)
+//		{
+//			printf("这个数是:%d ", i);
+//			break;
+//		}	
+//	}
+//	return 0;
+//}
